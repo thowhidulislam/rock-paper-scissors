03df817792c98ef0106a4fc95bfa34a67c74bab7
thowhidulislam <thowhid.ndc14@gmail.com>
2022-03-12 21:50:35 +0600

    user choice and computer choice added

---
+++ app.js
@@ -0,0 +1,28 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    const userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice()
+}))
+
+const generateComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
